ce21de3d18637c9f1c305f66d502d18369f04ea4
Asaraf Ali <asarafali066@gmail.com>
2026-08-24 23:35:05 +0530

    change

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://codearena-bly4.onrender.com',
         credentials: true
     }
 });
@@ -26,7 +26,7 @@ const battleRouter = require('./routes/battle');
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://codearena-bly4.onrender.com',
     credentials: true 
 }));
 
